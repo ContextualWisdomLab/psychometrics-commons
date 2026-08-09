91a184e0f6b6026674e5cdd667a541d0ddba6c83
Seongho Bae <me@seonghobae.me>
2026-08-10 01:53:55 +0900

    test: bind item-delivery value semantics to release manifest

--- tests/item_delivery_trait_contract.rs
+++ tests/item_delivery_trait_contract.rs
@@ -1,5 +1,6 @@
 //! Value-semantics tests for public item-delivery evidence types.
 
+use psychometrics_commons_runtime::instrument::InstrumentReleaseManifest;
 use psychometrics_commons_runtime::item_delivery::{
     ItemDeliveryError, ItemDeliveryLedger, ItemDeliveryRequest,
 };
@@ -10,6 +11,27 @@ const RELEASE_DIGEST: &str =
 
 fn clone_public_value<T: Clone>(value: &T) -> T {
     value.clone()
+}
+
+fn manifest() -> InstrumentReleaseManifest {
+    InstrumentReleaseManifest::new(
+        "release_big_five_ko_v1",
+        "instrument_big_five",
+        "instrument_version_ko_v1",
+        "construct_big_five",
+        &["item_version_001", "item_version_002"],
+        "ko-KR",
+        "assessment_spec_big_five_v1",
+        "scoring_big_five_v1",
+        "calibration_big_five_v1",
+        Some("norm_big_five_ko_v1"),
+        "narrative_big_five_v1",
+        &["consent_service_v1"],
+        "intended_use_self_reflection_v1",
+        "limitations_big_five_v1",
+        RELEASE_DIGEST,
+    )
+    .unwrap()
 }
 
 #[test]
@@ -26,13 +48,8 @@ fn cloned_public_values_preserve_delivery_evidence() {
         ItemDeliveryError::SessionNotActive(SessionState::Paused)
     );
 
-    let mut ledger = ItemDeliveryLedger::new(
-        "session_big_five_001",
-        "release_big_five_ko_v1",
-        RELEASE_DIGEST,
-        "ko-KR",
-    )
-    .unwrap();
+    let mut ledger =
+        ItemDeliveryLedger::from_manifest("session_big_five_001", &manifest()).unwrap();
     let event = ledger.deliver(SessionState::Active, request).unwrap();
 
     assert_eq!(clone_public_value(&event), event);
