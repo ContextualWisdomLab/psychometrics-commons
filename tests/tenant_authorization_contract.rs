995294d88e26a3179bb6444a9de7b3aa76ec167e
Seongho Bae <me@seonghobae.me>
2026-08-16 16:08:13 +0900

    test(authorization): stop relying on reference trimming

--- tests/tenant_authorization_contract.rs
+++ tests/tenant_authorization_contract.rs
@@ -325,9 +325,9 @@ fn malformed_or_numeric_references_fail_closed() {
 #[test]
 fn context_and_resource_metadata_are_auditable_without_identity_role_confusion() {
     let actor = AuthorizationContext::new(
-        " tenant_alpha ",
-        " subject_alpha ",
-        Some(" participant_alpha "),
+        "tenant_alpha",
+        "subject_alpha",
+        Some("participant_alpha"),
         &[
             ProductRole::Participant,
             ProductRole::Participant,
@@ -337,9 +337,9 @@ fn context_and_resource_metadata_are_auditable_without_identity_role_confusion()
     .unwrap();
     let resource = ResourceScope::participant_owned(
         ResourceKind::Result,
-        " tenant_alpha ",
-        " participant_alpha ",
-        " result_alpha ",
+        "tenant_alpha",
+        "participant_alpha",
+        "result_alpha",
     )
     .unwrap();
 
