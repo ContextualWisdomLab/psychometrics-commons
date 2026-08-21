3a1e7a9803cdecbcd01dff48467185fb65d20000
Seongho Bae <me@seonghobae.me>
2026-08-21 01:52:24 -0700

    test(health): reject case-folded relation aliases

--- tests/postgres_runtime_readiness.rs
+++ tests/postgres_runtime_readiness.rs
@@ -124,6 +124,14 @@ fn relation_integrity_probe_rejects_search_path_relative_relation_names() {
 }
 
 #[test]
+fn relation_integrity_probe_rejects_case_folded_relation_aliases() {
+    let mut client = test_client();
+    let integrity = probe_postgres_relation_integrity(&mut client, &["PG_CATALOG.PG_CLASS"]).unwrap();
+
+    assert_eq!(integrity, DataIntegrityHealth::Incompatible);
+}
+
+#[test]
 fn relation_integrity_probe_fails_closed_when_a_required_relation_is_missing() {
     let mut client = test_client();
     let integrity = probe_postgres_relation_integrity(
