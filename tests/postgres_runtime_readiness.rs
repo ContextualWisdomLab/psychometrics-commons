59c9bca8650111c3062c394a866b8a9745c86879
Seongho Bae <me@seonghobae.me>
2026-08-21 01:55:26 -0700

    test(health): reject malformed qualified relation names

--- tests/postgres_runtime_readiness.rs
+++ tests/postgres_runtime_readiness.rs
@@ -132,6 +132,15 @@ fn relation_integrity_probe_rejects_case_folded_relation_aliases() {
 }
 
 #[test]
+fn relation_integrity_probe_rejects_whitespace_and_three_part_names() {
+    let mut client = test_client();
+    for relation in ["pg_catalog. pg_class", "pg_catalog.pg_class.extra"] {
+        let integrity = probe_postgres_relation_integrity(&mut client, &[relation]).unwrap();
+        assert_eq!(integrity, DataIntegrityHealth::Incompatible, "{relation}");
+    }
+}
+
+#[test]
 fn relation_integrity_probe_fails_closed_when_a_required_relation_is_missing() {
     let mut client = test_client();
     let integrity = probe_postgres_relation_integrity(
